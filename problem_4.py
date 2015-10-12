fbc1859c827a1b1421c5bb9694b7c5d7524c4b81
Wes Evans <euphwes@gmail.com>
2015-10-12 16:27:46 -0600

    Added solution to Problem 4.

---
+++ problem_4.py
@@ -0,0 +1,34 @@
+"""
+Largest palindrome product
+--------------------------
+
+A palindromic number reads the same both ways. The largest palindrome made from the product of two
+2-digit numbers is 9009 = 91 × 99.
+
+Find the largest palindrome made from the product of two 3-digit numbers.
+"""
+
+from utils.timer import timeit
+
+#-------------------------------------------------------------------------------------------------
+
+def is_palindome(val):
+    """ Returns whether or not a value is a palindome. For non-string arguments, they are first
+    converted to their string representation. """
+
+    if type(val) is not str:
+        val = str(val)
+    return val == ''.join(reversed(val))
+
+
+@timeit
+def problem_4():
+    three_digit_products = (int(x * y) for x in range(100,1000) for y in range(100,1000))
+    palindrome_products = filter(is_palindome, three_digit_products)
+    print(max(palindrome_products))
+
+#-------------------------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    print()
+    problem_4()
