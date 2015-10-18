3ba6e49862fbb7eafe86bb06699d12f7a13c0883
Wes Evans <euphwes@gmail.com>
2015-10-18 17:05:02 -0600

    Stub for Problem 26. Added first draft of utils.math.long_division, which performs division of two integers, limited up to a certain number of decimals. Next version will detect repetition.

--- problems/utils/math.py
+++ problems/utils/math.py
@@ -23,3 +23,50 @@ def proper_divisors(n):
     # Weed out the duplicate divisors by running the list through a set, and then sort the set
     # to get a sorted list back
     return sorted(set(divisors))
+
+#-------------------------------------------------------------------------------------------------
+
+def long_divison(n, d, digit_limit=10):
+    """ Calculates n/d (numerator/denominator) by long division. Returns a string representation
+    of the result, up to `digit_limit` digits. """
+
+    # Performs integer division on n / d to get the whole number part of the solution
+    # Calculate the "new numerator" to be used in the next step of the long division, by taking
+    # 10 * (the difference between the current numerator and denominator * result of last step)
+    i = n // d
+    n = 10 * (n - i * d)
+
+    # If n == 0, the solution was a whole number, and we can return that immediately
+    if n == 0:
+        return str(i)
+
+    # If we still have a numerator, we're going to have a decimal portion
+    # Note the whole number portion as i so we can concatenate it with the decimals when we're
+    # done with our calculations
+    whole = str(i)
+
+    # Holds the decimal portion's digits of the solution. Convert this into a string when returned
+    decimals = ''
+
+    while len(decimals) < digit_limit:
+        # Continue long division like described above. Perform integer division on current
+        # numerator and denominator, get result, and figure out new numerator based on difference
+        i = n // d
+        n = 10 * (n - i * d)
+
+        # Append the current result to the decimal portion
+        decimals += str(i)
+
+        # If numerator is now 0, the division was clean, and we're done
+        if n == 0:
+            break
+
+    # Format the full solution with whole number portion + decimal portion
+    solution = '{}.{}'.format(whole, decimals)
+
+    # If we still have a numerator left, we hit the digit limit. Append '...' to the result so
+    # that the caller knows the solution continues
+    if n > 0:
+        return solution + '...'
+
+    return solution
